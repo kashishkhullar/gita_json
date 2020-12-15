e3a343db866796fae6776511791c3062da63eb52
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2020-12-15 17:44:16 +0530

    "created chapter verse class"

---
+++ chapter.py
@@ -0,0 +1,9 @@
+class Chapter:
+    def __init__(self):
+        self.chapter_number = ''
+        self.chapter_summary = ''
+        self.name = ''
+        self.name_meaning = ''
+        self.name_translation = ''
+        self.name_transliterated = ''
+        self.verses_count = ''
---
+++ verse.py
@@ -0,0 +1,7 @@
+class Verse:
+    def __init__(self):
+        self.meaning = ''
+        self.text = ''
+        self.transliteration = ''
+        self.verse_number = ''
+        self.word_meanings = ''
