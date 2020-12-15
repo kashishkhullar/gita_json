b8cb45507b1e6802d451b45c0d08dfc87ad2ffae
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2020-12-15 19:23:43 +0530

    "removed extra fields"

--- Model/Verse.py
+++ Model/Verse.py
@@ -2,6 +2,5 @@ class Verse:
     def __init__(self):
         self.meaning = ''
         self.text = ''
-        self.transliteration = ''
         self.verse_number = ''
         self.word_meanings = ''
