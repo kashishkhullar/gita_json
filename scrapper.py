3526825a798593c0a1d6ae0deb98c5853639b3d8
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2020-12-15 22:07:12 +0530

    added code to scrap verses

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,4 @@
-from Model import Chapter
+from Model import Chapter, Verse
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -10,12 +10,11 @@ data["verses"] = {}
 
 """
 make a request to each chapter
-get all the relevant details of the chapter in a list
-store list in the data
-go through the list
+get all the details of the chapter in an object
+add chapter to corresponding number in the data object
 make a request to each verse
 get all the details of the verse
-store the verses for the chapter in a list
+store the verses for the chapter number and corresponding verse number in the data object
 
 """
 
@@ -24,8 +23,18 @@ hindi_numbers = ["०", "१", "२", "३", "४", "५", "६", "७", "८", 
 
 chapter_hindi = "अध्याय"
 verse_hindi = "श्लोक"
+verse_meaning_hindi = "अनुवाद"
+word_meaning_hindi = "शब्दार्थ"
 
 chapters = data["chapters"]
+verses = data["verses"]
+
+
+def convertNumberToHindi(num):
+    hindi = ""
+    num = num.split("-")
+    hindi_list = map(lambda number: hindi_numbers[int(number)], num)
+    return "-".join(hindi_list)
 
 
 # Getting details of the chapters
@@ -37,10 +46,70 @@ for chapter_number in range(1, 19):
     soup = BeautifulSoup(page.content, "html.parser")
 
     chapter = Chapter()
-    chapter.chapter_number = hindi_numbers[1]
+    chapter.chapter_number = hindi_numbers[chapter_number]
     chapter.chapter_summary = soup.find("p").text
     chapter.name = soup.find("b").text.split(" ")[-1:]
     chapter.name_meaning = soup.find("h3").text
+    chapter.verse_numbers = []
+
+    no_of_verses = 0
+    verses[chapter_number] = {}
+
+    # Getting all verse numbers ( some have two combined )
+
+    page_no = 1
+    has_more_pages = True
+
+    while has_more_pages:
+        url = "https://bhagavadgita.io/chapter/" + \
+            str(chapter_number) + "/hi/" + "?page=" + str(page_no)
+
+        page = requests.get(url)
+
+        if(page.status_code == 200):
+            print("Getting verse numbers from page", page_no)
+            soup = BeautifulSoup(page.content, "html.parser")
+            verse_number_element = soup.find_all(
+                "h4", {"class": "font-up font-bold white-text mt-2 mb-3"})
+            for element in verse_number_element:
+                verse_number = element.text.split(" ")[-1]
+                chapter.verse_numbers.append(verse_number)
+                no_of_verses += 1
+                verses[chapter_number][verse_number] = {}
+            page_no += 1
+        else:
+            has_more_pages = False
+            chapter.verses_count = no_of_verses
+
+    # Get all verses for the chapters
+    has_more_verses = True
+
+    for verse_number in chapter.verse_numbers:
+        url = "https://bhagavadgita.io/chapter/" + \
+            str(chapter_number) + "/verse/" + str(verse_number) + "/hi/"
+
+        page = requests.get(url)
+
+        if(page.status_code == 200):
+            print("Getting verse:", verse_number)
+
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            verse = Verse()
+            verse.text = soup.find("p", {"class": "verse-sanskrit"}).text
+            verse.meaning = soup.find("p", {"class": "verse-meaning"}).text
+            verse.word_meanings = soup.find(
+                "p", {"class": "verse-word"}).text
+            verse.verse_number = convertNumberToHindi(verse_number)
+
+            verses[chapter_number][verse_number] = vars(verse)
+
+        else:
+            print("No more verses for chapter", chapter_number)
+            has_more_verses = False
+            chapter.verses_count = no_of_verses
+
+    # converting to dict before adding to data to serialize it
     chapters[chapter_number] = vars(chapter)
 
 
@@ -51,6 +120,6 @@ for chapter_number in range(1, 19):
 #     obj["name"] = i.text
 #     data.append(obj)
 
-
-file1 = open("MyFile2.json", "w", encoding="utf-8")
-json.dump(data, file1, ensure_ascii=False)
+# Writing the data to json file
+data_file = open("dataset.json", "w", encoding="utf-8")
+json.dump(data, data_file, ensure_ascii=False)
